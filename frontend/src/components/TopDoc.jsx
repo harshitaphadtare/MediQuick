adf2f641946dc1516bc286a09bd7fce77efd9cf9
Harshita <hphadtare2023@gmail.com>
2025-02-19 02:03:55 +0530

    complete doctors section

--- frontend/src/components/TopDoc.jsx
+++ frontend/src/components/TopDoc.jsx
@@ -6,10 +6,15 @@ const TopDoc = () => {
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
       <p className="sm:w-1/3 text-center text-sm">
-        Simply browse through our extensive list of trusted doctors, schedule
-        your appointment hassle-free.
+        Simply browse through our extensive list of trusted doctors.
       </p>
-      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+        }}
+        className="w-full gap-4 pt-7 gap-y-6 px-3 sm:px-0"
+      >
         {doctors.slice(0, 10).map((doctor, index) => (
           <div
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
@@ -22,14 +27,18 @@ const TopDoc = () => {
                 <p>Available</p>
               </div>
               <div>
-                <p>{doctor.name}</p>
-                <p>{doctor.speciality}</p>
+                <p className="text-gray-900 text-lg font-medium">
+                  {doctor.name}
+                </p>
+                <p className="text-gray-600 text-sm">{doctor.speciality}</p>
               </div>
             </div>
           </div>
         ))}
       </div>
-      <button>more</button>
+      <button className="bg-blue-50 text-gray-600 px-8 py-3 rounded-full mt-10">
+        more
+      </button>
     </div>
   );
 };
