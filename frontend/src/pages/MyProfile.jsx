7c162aaa246f45deaff57d096c5bd3940511b677
Harshita <hphadtare2023@gmail.com>
2025-02-24 22:36:59 +0530

    started my profile section

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const MyProfile = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: "richarjamesswap@gmail.com",
+    phone: "+1  123 456 7890",
+    address: {
+      line1: "57th Cross, Richmond ",
+      line2: "Circle, Church Road, London",
+    },
+    gender: "Male",
+    dob: "2000-01-01",
+  });
+  return <div></div>;
+};
 
-export default MyProfile
+export default MyProfile;
