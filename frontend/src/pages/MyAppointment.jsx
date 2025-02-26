09d9f5d647d2433eeae51dbab305aac8e3c14dd2
Harshita <hphadtare2023@gmail.com>
2025-02-26 22:19:52 +0530

    started with my appointment page

--- frontend/src/pages/MyAppointment.jsx
+++ frontend/src/pages/MyAppointment.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const MyAppointment = () => {
+  const { doctors } = useContext(AppContext);
   return (
     <div>
-        
+      <p>My Appointment</p>
+      <div>{doctors.slice(0, 2).map((item, index) => {})}</div>
     </div>
-  )
-}
+  );
+};
 
-export default MyAppointment
+export default MyAppointment;
